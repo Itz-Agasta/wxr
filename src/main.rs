9613c129d7ae213460991add29d463e3d6f149d9
Itz-Agasta <rupamgolui69@gmail.com>
2026-01-12 17:33:06 +0530

    init commit and plan

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+// wxr --configure --> like aws cli we will take ask user 1 time queions 1. his location 2.
+// openwatehr api key....
+//
+// wxr --> will print! the whole weather info (temp, aql etc etc)
+//
+// wxr -temp,aqi --> will print only the flags.
+
+
+
+
+// get req to openwatehr
+
+
+
+// fun to get users info for making the config file in his dir.
+// Res hwo aws-cli stores this info in user's system.
+
+
+// main fun that will take flags
